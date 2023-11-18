e7c8dff7f6a14c92d78d79d9d6c8acbac11c9200
AHSAN SIDDZ <ahsansiddz1@gmail.com>
2023-11-19 02:45:42 +0500

    Initial Commit

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -27,12 +27,12 @@ const imageAltText = "Picture of Ahsan Siddiqui";
 const Home = ({ name, title }) => {
   return (
     <section id="home" className="min-height">
-      <img className="background" src={image} alt="" />
-      <div style={{ position: "relative", top: "105rem", left: "2rem", width: "7rem" }}>
+      <img className="background" src={image} alt="" style={{ width: '100%' }} />
+      <div style={{ position: "absolute", top: "5rem", left: "2rem", width: "17rem" }}>
         <h1>{name}</h1>
         <h2>{title}</h2>
       </div>
-      <div style={{ position: "relative", bottom: "3rem", left: "50%" }}>
+      <div style={{ position: "absolute", bottom: "3rem", left: "50%" }}>
         <img src={arrowSvg} style={{ height: "3rem", width: "3rem" }} alt={imageAltText} />
       </div>
     </section>
@@ -41,7 +41,7 @@ const Home = ({ name, title }) => {
 
 Home.defaultProps = {
   name: "Ahsan Siddiqui",
-  title: "Engr.",
+  title: "Engr",
 };
 
 Home.propTypes = {
